069bf3fc5a59f3d77289fd8f0ef35052fdcc3345
Developer Mallon <timafra4@mallon.com.br>
2023-11-25 09:36:58 -0300

    teste

--- install.bat
+++ install.bat
@@ -1,4 +1,7 @@
 python -m venv venv
-.\\venv\\Scripts\\activate
-pip install -r .\\requirements.txt
+.\venv\Scripts\activate
+pip install -r requirements.txt
 pre-commit install
+
+@REM $env:FLASK_APP = "run.py"
+@REM flask run
