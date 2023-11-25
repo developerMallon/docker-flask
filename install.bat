1cabe9fb841e9c6a46b5d86ec2c3355f51e0a33a
Developer Mallon <timafra4@mallon.com.br>
2023-11-25 09:40:00 -0300

    teste

--- install.bat
+++ install.bat
@@ -3,5 +3,8 @@ python -m venv venv
 pip install -r requirements.txt
 pre-commit install
 
+@REM Use os comandos abaixo para rodar o projeto.
 @REM $env:FLASK_APP = "run.py"
 @REM flask run
+
+@REM  & d:/devs/python/docker/docker-flask/venv/Scripts/Activate.ps1 ---- testar esse comando
