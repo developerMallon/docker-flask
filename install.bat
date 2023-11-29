bf37cbeccb198959f13359dc10c7dcbf135d32b1
DeveloperMallon <timafra4@mallon.com.br>
2023-11-29 16:24:22 -0300

    env: changes values to implements in mallon nb

--- install.bat
+++ install.bat
@@ -1,7 +1,20 @@
+@echo off
+
+REM Criação do ambiente virtual
 python -m venv venv
-.\venv\Scripts\activate
+
+REM Ativação do ambiente virtual
+call .\venv\Scripts\activate
+
+REM Instalação das dependências
 pip install -r requirements.txt
+
+REM Instalação do pre-commit
 pre-commit install
+
+echo Setup concluído. Pressione qualquer tecla para fechar.
+pause
+
 
 @REM Use os comandos abaixo para rodar o projeto.
 @REM $env:FLASK_APP = "run.py"
